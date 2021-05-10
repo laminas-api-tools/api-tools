b8a58eb05949663a89ac06b8ca7ce36c19629431
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2021-05-10 02:11:22 -0400

    Remove file headers
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- test/TestAsset/Bar.php
+++ test/TestAsset/Bar.php
@@ -1,11 +1,5 @@
 <?php
 
-/**
- * @see       https://github.com/laminas-api-tools/api-tools for the canonical source repository
- * @copyright https://github.com/laminas-api-tools/api-tools/blob/master/COPYRIGHT.md
- * @license   https://github.com/laminas-api-tools/api-tools/blob/master/LICENSE.md New BSD License
- */
-
 namespace LaminasTest\ApiTools\TestAsset;
 
 class Bar
